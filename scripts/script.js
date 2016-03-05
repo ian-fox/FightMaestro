58157c27ee15798ec77504a2d33c7ada8b68de80
Alexander Zvorygin <livingalex@gmail.com>
2016-03-04 20:46:47 -0500

    dots moving across screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,52 @@
-
-var map = 0;
+var beatMap = [];
+var startTime = 0;
 var MAP_LENGTH = 20000;
+var MS_PER_BEAT = 1000;
+var gestures = ["fist", "spread", "wave-in", "wave-out"];
 
-generateMap () {
-    if (!map) {
-        for (int i =0; i < MAP_LENGTH; i+=100){ 
-            if (i %1000 ==0) {
+function Beat(hitTime, gesture) {
+    //Create a Shape DisplayObject.
+    this.circle = new createjs.Shape();
+    this.circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    //Set position of Shape instance.
+    //Add Shape instance to stage display list.
+    stage.addChild(this.circle);
+    //Update stage will render next frame
+    this.hitTime= hitTime;
+    this.gesture = gesture;
+    this.circle.y = 100+ 100 * gestures.indexOf(gesture);
+    this.setX = function () {
+        if (this.circle.x >  -500){
+            this.circle.x = startTime + this.hitTime - new Date().getTime();
+        }
+    }
+    this.setX();
+}
 
-
-            }
+function generateMap () {
+    if (!beatMap.length) {
+        for (var i =0; i < MAP_LENGTH; i+=MS_PER_BEAT){ 
+            var num = Math.floor(Math.random() * 4);
+            var beat = new Beat (i, gestures[num]);
+            beatMap.push(beat);
         }
     }
 }
 
-
-function init () {
-    generateMap();
-
-
-
-
+function drawBeats () {
+    beatMap.forEach(function (entry) {
+        entry.setX();
+    });
+    stage.update();
 }
 
-window.onload = function () {
-    init();
+
+function init () {
     //Create a stage by getting a reference to the canvas
     stage = new createjs.Stage("canvas");
-    //Create a Shape DisplayObject.
-    circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
-    //Set position of Shape instance.
-    circle.x = circle.y = 50;
-    //Add Shape instance to stage display list.
-    stage.addChild(circle);
-    //Update stage will render next frame
-    stage.update();
-};
+    startTime = new Date().getTime();
+    generateMap();
+    createjs.Ticker.addEventListener("tick", drawBeats);
+}
+
+window.onload = init;
