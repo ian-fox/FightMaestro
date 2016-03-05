db65a79afae869845546a52cd1c494c878af6648
Alexander Zvorygin <livingalex@gmail.com>
2016-03-04 22:41:50 -0500

    pivot

--- scripts/script.js
+++ scripts/script.js
@@ -5,54 +5,53 @@ var FONT = "share-regular,'Arial Narrow',sans-serif";
 var CANVAS_WIDTH= 900; // width of canvas
 var MS_PER_BEAT = 1000; 
 var CANVAS_HEIGHT= 900; // width of canvas
+var CAR_IMAGE=0;
 var LINE_HEIGHT = 100; // height of a single "line" of incoming gestures
 var START_DELAY = 3000; // delay until the game starts var MS_PER_BEAT = 1000; 
-var gestures = ["fist", "shield", "lighting", "fireball"];
+var badguys= ["fist", "lighting", "fireball"];
+var lanes = 3;
 var guides = [];// set of horizontal line guides for showing incoming jazz
 
-function Beat(hitTime, gesture) {
+function Beat(hitTime, gesture, lane) {
     //Create a Shape DisplayObject.
-    this.car= new createjs.Bitmap("res/car.png");
-    this.car.scaleY=0.3;
-    this.car.scaleX=0.3;
+    this.enemy= new createjs.Bitmap("res/car.png");
+    this.enemy.scaleY=0.3;
+    this.enemy.scaleX=0.3;
     //Add Shape instance to stage display list.
-    stage.addChild(this.car);
+    stage.addChild(this.enemy);
     //Update stage will render next frame
     this.hitTime= hitTime;
     this.gesture = gesture;
-    this.car.y = 200+ 100 * gestures.indexOf(gesture);
-    this.car.x = 20000;//offscreen hack
+    this.lane = lane;
+    this.enemy.y = 200 + 100*lane;
+    this.enemy.x = CANVAS_WIDTH + 2000;//offscreen hack
     this.setX = function () {
-        if (this.car.x > -500){
-            this.car.x = 200+ startTime + this.hitTime - new Date().getTime();
+        if (this.enemy.x > -500){
+            this.enemy.x = 200+ startTime + this.hitTime - new Date().getTime();
         }
     }
     this.setX();
 }
 
 function drawGuides(){
-    for (var i = 1; i < (gestures.length +2); ++i ){ 
+    if (guides.length > 0){
+        guides.length =0;
+    }
+    for (var i = 2; i<= 2+badguys.length; i+=badguys.length ){ 
         var rect = new createjs.Shape();
-        var color = (i == 1 || i == 5) ? "white" : "yellow";
-        rect.graphics.beginFill(color).drawRect(0,92+i*LINE_HEIGHT, CANVAS_WIDTH, 5);
+        rect.graphics.beginFill("black").drawRect(0,i*LINE_HEIGHT, CANVAS_WIDTH, 5);
         stage.addChild(rect);
         guides.push(rect);
-
     }
-
 }
 
-function drawBackground(){
-    var rect = new createjs.Shape();
-    rect.graphics.beginFill("#444444").drawRect(0,200, CANVAS_WIDTH, 5*LINE_HEIGHT);
-    //stage.addChild(rect);
-}
 
 function generateMap () {
     if (!beatMap.length) {
-        for (var i =0; i < MAP_LENGTH; i+=MS_PER_BEAT* Math.max(Math.random(),0.5)){
-            var num = Math.floor(Math.random() * 4);
-            var beat = new Beat (i + START_DELAY, gestures[num]);
+        for (var i =0; i < MAP_LENGTH; i+=MS_PER_BEAT* Math.max(Math.random()*3,1)){
+            var lane= Math.floor(Math.random() * 3);
+            var type= Math.floor(Math.random() * 3);
+            var beat = new Beat (i + START_DELAY, badguys[type], lane);
             beatMap.push(beat);
         }
     }
@@ -67,11 +66,23 @@ function drawBeats () {
 
 
 function init () {
-    //Create a stage by getting a reference to the canvas
+    var canvas = document.getElementById("canvas");
     stage = new createjs.Stage("canvas");
+    function resizeCanvas (){
+        canvas.width=window.innerWidth-20;
+        canvas.height=window.innerHeight-20;
+        CANVAS_WIDTH=canvas.width;
+        CANVAS_HEIGHT=canvas.height;
+        drawGuides();
+    }
+    resizeCanvas();
+
+    window.addEventListener('resize', resizeCanvas, false);
+
+    //Create a stage by getting a reference to the canvas
     startTime = new Date().getTime();
     generateMap();
-    drawBackground();
+    //drawBackground();
     drawGuides();
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", drawBeats);
